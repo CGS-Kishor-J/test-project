fedf399b7ef71253322b5e489d3df9fbfaedae89
Kishor J <kishor.j@cgs.act.edu.au>
2015-07-30 15:08:34 +1000

    New Fix

---
+++ testProject/testProject/ViewController2.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController2.swift
+//  testProject
+//
+//  Created by Kishor J on 30/07/2015.
+//  Copyright (c) 2015 kCode. All rights reserved.
+//
+
+import Foundation
+
+println("Hi")
